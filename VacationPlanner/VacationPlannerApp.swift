21ec5a4ef020c823166b9f9b4dca81bea30b0389
Mikhail <mich_p@inbox.ru>
2023-05-14 09:02:22 +0300

    Initial Commit

---
+++ VacationPlanner/VacationPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationPlannerApp.swift
+//  VacationPlanner
+//
+//  Created by Миша Перевозчиков on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct VacationPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
